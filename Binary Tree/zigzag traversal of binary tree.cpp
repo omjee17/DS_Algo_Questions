689a238fae8749248bc92c651ab5d2780f0b7255
omjee17 <singhomjee123@gmail.com>
2021-07-14 15:50:26 +0530

    Questions

---
+++ Binary Tree/zigzag traversal of binary tree.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+ 
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+vector <int> zigZagTraversal(Node* root)
+{
+    vector<int>ans;
+    
+    stack<Node*>s1;
+    stack<Node*>s2;
+    
+    s1.push(root);
+    
+    while(!s1.empty())
+    {
+        Node*a=s1.top();
+        while(!s1.empty())
+        {
+            ans.push_back(a->data);
+            if(a->left)
+            s2.push(a->left);
+            if(a->right)
+            s2.push(a->right);
+            
+            s1.pop();
+            if(!s1.empty())
+            a=s1.top();
+        }
+        
+        
+        if(!s2.empty())
+        {
+            Node*a1=s2.top();
+            while(!s2.empty())
+            {
+                ans.push_back(a1->data);
+                if(a1->right)
+                s1.push(a1->right);
+                if(a1->left)
+                s1.push(a1->left);
+                
+                s2.pop();
+                if(!s2.empty())
+                {
+                    a1=s2.top();
+                }
+                
+            }
+            
+        }
+        
+    }
+    return ans;
+   
+}
+
+
+int main()
+{
+
+    return 0;
+}
