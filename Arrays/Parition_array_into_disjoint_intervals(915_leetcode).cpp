7c987e66bae6b61ad3e567f3c7f28ef6f077ba3c
omjee17 <singhomjee123@gmail.com>
2021-07-22 14:24:21 +0530

    Questions

---
+++ Arrays/Parition_array_into_disjoint_intervals(915_leetcode).cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// return minimum size of left array such that 
+// left and right are two non-empty subarray
+// in which every element in left is less than or equal to element in right
+
+int partitionDisjoint(vector<int>& nums) 
+{
+        int maxInLeft=nums[0],maxOverall=nums[0];
+        int paritionIndex=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<maxInLeft)
+            {
+                paritionIndex=i;
+                maxInLeft=maxOverall;
+            }
+            
+            maxOverall=max(nums[i],maxOverall);
+        }
+    return paritionIndex+1;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    cout<<partitionDisjoint(v)<<endl;
+
+    return 0;
+}
